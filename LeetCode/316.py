0e3a5a8cca5d17e6aa1f3bf96925c3f827397539
Soyeon Hong <72512692+SoyeonHH@users.noreply.github.com>
2021-05-30 23:38:06 +0900

    Add files via upload

---
+++ LeetCode/316.py
@@ -0,0 +1,29 @@
+import collections
+
+class Solution:
+
+    # 재귀를 이용해 분리하기
+    def removeDuplicateLetters_1(self, s: str) -> str:
+        # 집합으로 정렬한 순서대로 진행
+        for char in sorted(set(s)):
+            suffix = s[s.index(char):]
+            # 전체 집합과 접미사 집합이 일치할 때 분리 진행
+            if set(s) == set(suffix):
+                return char + self.removeDuplicateLetters(suffix.replace(char,''))
+        return ''
+
+    # 스택
+    def removeDuplicateLetters_2(self, s: str) -> str:
+        counter, seen, stack = collections.Counter(s), set(), []
+
+        for char in s:
+            counter[char] -= 1
+            if char in seen:
+                continue
+            # 뒤에 붙일 문자가 남아 있다면 스택에서 제거
+            while stack and char < stack[-1] and counter[stack[-1]] > 0:
+                seen.remove(stack.pop())
+            stack.append(char)
+            seen.add(char)
+
+        return ''.join(stack)
