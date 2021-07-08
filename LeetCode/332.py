f5496b66356e778a2a34826c68ba2d6a48ea0418
Soyeon Hong <72512692+SoyeonHH@users.noreply.github.com>
2021-07-08 13:25:17 +0900

    Add files via upload

---
+++ LeetCode/332.py
@@ -0,0 +1,18 @@
+import collections
+
+class Solution:
+    def findItinerary(self, tickets: list[list[str]]) -> list[str]:
+        graph = collections.defaultdict(list)
+
+        for a, b in sorted(tickets, reverse=True):
+            graph[a].append(b)
+
+        route = []
+
+        def dfs(a):
+            while graph[a]:
+                dfs(graph[a].pop())
+            route.append(a)
+
+        dfs('JFK')
+        return route[::-1]
