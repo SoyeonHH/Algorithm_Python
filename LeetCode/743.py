ddb1e2cf6a8a20794e6a8b8340afbce1d3b497c2
Soyeon Hong <72512692+SoyeonHH@users.noreply.github.com>
2021-07-11 23:48:35 +0900

    Add files via upload

---
+++ LeetCode/743.py
@@ -0,0 +1,26 @@
+import collections
+
+class Solution:
+    def networkDelayTime(self, times: list[list[int]], n: int, k: int) -> int:
+        graph = collections.defaultdict(list)
+        # 그래프 인접 리스트 구성
+        for u, v, w in times:
+            graph[u].append((v, w))
+
+        # 큐 변수 : [(소요 시간, 정점)]
+        Q = [(0, k)]
+        dist = collections.defaultdict(int)
+
+        # 우선순위 큐 최솟값 기준으로 정점까지 경로 삽입
+        while Q:
+            time, node = heapq.heappop(Q)
+            if node not in dist:
+                dist[node] = time
+                for v, w in graph[node]:
+                    alt = time + w
+                    heapq.heappush(Q, (alt, v))
+
+        # 모든 노드의 최단 경로 존재 여부 판별
+        if len(dist) == n:
+            return max(dist.values())
+        return -1
