d9b3c469c12aaf20f9a40dbc3f8dde676311a19f
Soyeon Hong <72512692+SoyeonHH@users.noreply.github.com>
2021-07-18 23:01:34 +0900

    Add files via upload

---
+++ LeetCode/297.py
@@ -0,0 +1,59 @@
+import collections
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Codec:
+    # 직렬화
+    def serialize(self, root: TreeNode) -> str:
+        queue = collections.deque([root])
+        result = ['#']
+
+        while queue:
+            node = queue.popleft()
+            if node:
+                queue.append(node.left)
+                queue.append(node.right)
+
+                result.append(str(node.val))
+            else:
+                # None을 문자열로 만들 수 없으므로 null값의 의미로 #을 넣어준다.
+                result.append('#')
+
+        # result는 리스트(list)가 아닌 배열(str)로 바꿔준다.
+        return ' '.join(result)
+
+    # 역직렬화
+    def deserialize(self, data: str) -> TreeNode:
+        # 예외 처리
+        if data == '# #':
+            return None
+
+        nodes = data.split()  # 리스트 변수로 변경
+
+        root = TreeNode(int(nodes[1]))
+        queue = collections.deque([root])
+
+        index = 2
+        while queue:
+            node = queue.popleft()
+            if nodes[index] is not '#':
+                node.left = TreeNode(int(nodes[index]))
+                queue.append(node.left)
+            index += 1
+
+            if nodes[index] is not '#':
+                node.right = TreeNode(int(nodes[index]))
+                queue.append(node.right)
+            index += 1
+
+        return root
+
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# ans = deser.deserialize(ser.serialize(root))
