0928765cf78a8f3245f702b2a6bd151d97cb7026
Soyeon Hong <72512692+SoyeonHH@users.noreply.github.com>
2021-07-27 10:44:36 +0900

    Add files via upload

---
+++ LeetCode/938.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
+        def dfs(node: TreeNode):
+            if not node:
+                return 0
+
+            if node.val < low:
+                return dfs(node.right)
+            elif node.val > high:
+                return dfs(node.left)
+            return node.val + dfs(node.left) + dfs(node.right)
+
+        return dfs(root)
+
+    def rangeSumBST_iteration(self, root: TreeNode, low: int, high: int) -> int:
+        stack, sum = [root], 0
+        # 스택 이용 필요한 노드 DFS 반복
+        while stack:
+            node = stack.pop()
+            if node:
+                if node.val > low:
+                    stack.append(node.left)
+                if node.val < high:
+                    stack.append(node.right)
+                if low <= node.val <= high:
+                    sum += node.val
+                    
+        return sum
