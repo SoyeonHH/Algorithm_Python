a3873ac606722b8663a5f44432d6694363c290cf
Soyeon Hong <72512692+SoyeonHH@users.noreply.github.com>
2021-08-09 22:33:22 +0900

    Add files via upload

---
+++ LeetCode/147.py
@@ -0,0 +1,19 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def insertionSortList(self, head: ListNode) -> ListNode:
+        cur = parent = ListNode(0)
+        while head:
+            while cur.next and cur.next.val < head.val:
+                cur = cur.next
+
+            cur.next, head.next, head = head, cur.next, head.next
+
+            # 필요한 경우에만 cur 포인터가 되돌아가도록 처리
+            if head and cur.val > head.val:
+                cur = parent
+        return parent.next
